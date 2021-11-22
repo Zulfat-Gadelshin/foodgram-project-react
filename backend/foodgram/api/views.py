a9c60d9fa907c52233aa01c1711319113f4a4e62
Zulfat-Gadelshin <zulfatgadelshin0106@gmail.com>
2021-11-22 21:48:42 +0300

    add custom pagination.py

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,30 +3,30 @@ from .models import *
 from .serializers import *
 from .filters import IngredientFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import CustomPageLimitPagination
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    pagination_class = None
+    pagination_class = CustomPageLimitPagination
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    pagination_class = None
     filter_backends = [DjangoFilterBackend, ]
-    #filterset_fields = ['name']
     filter_class = IngredientFilter
-    #search_fields = ('^name',)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = None
+    pagination_class = CustomPageLimitPagination
 
 
 class IngredientInRecipeViewSet(viewsets.ModelViewSet):
     queryset = IngredientInRecipe.objects.all()
     serializer_class = IngredientInRecipeSerializer
+    pagination_class = CustomPageLimitPagination
