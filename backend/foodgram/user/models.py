cf4be125e126309c77735eca2ee0f2471658007e
zulfatgadelshin <zulfatgadelshin0106@gmail.com>
2022-01-16 14:34:24 +0300

    change user/models.py

--- backend/foodgram/user/models.py
+++ backend/foodgram/user/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import AbstractUser
+
+User = get_user_model()
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(User):
     class Roles(models.TextChoices):
         ADMIN = 'admin'
         USER = 'user'
